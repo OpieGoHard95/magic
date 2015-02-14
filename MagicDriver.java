89d00cd01ef5136a468e86402609f4f7b191926d
OpieGoHard95 <jsmith2@saintjoe.edu>
2015-02-14 15:34:09 -0500

    Needed to test the common case constructor :)

--- MagicDriver.java
+++ MagicDriver.java
@@ -2,5 +2,7 @@ public class MagicDriver {
     public static void main(String [] args) {
 	Magic card = new Magic();
 	System.out.println(card.toString());
+	Magic vexingDevil = new Magic("Vexing Devil", "Avacyn Restored", "R", "Creature");
+	System.out.println(vexingDevil.toString());
     }
 }
